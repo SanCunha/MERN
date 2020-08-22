35317258b9c92a058aa7527b31c2bdbf5109f793
San <san.cunha@upflow.me>
2020-08-22 11:54:37 -0300

    Adicionando CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+  crossorigin="anonymous"
+/>
